cce8597e10808c695a7ff7a5052b8714f640167f
william1616 <william1616@users.noreply.github.com>
2013-10-03 22:51:10 +0100

    added money counter to gui fixed width (sortoff)
    wraptext needs fixing

--- tk test.py
+++ tk test.py
@@ -13,7 +13,6 @@ cntQuestions = 0
 correct = 0
 cntRounds = 1
 cntRquestions = 1
-bank = 0
 
 def correctAns():
     questionHandler(1)
@@ -23,33 +22,54 @@ def incorrectAns():
     questionHandler(2)
     askQuestion()
     
-def time():
-    questionHandler(3)
-    
-def bank():
+def timeEvent():
     questionHandler(4)
+    askQuestion()
+    
+def bankEvent():
+    questionHandler(3)
+
+def start():
+    global startB, correctB, incorrectB, timeB, bankB
+    startB.grid_forget()
+    correctB.grid(column=2, row=1, sticky=N)
+    incorrectB.grid(column=2, row=2, sticky=N)
+    timeB.grid(column=3, row=1, sticky=N)
+    bankB.grid(column=3, row=2, sticky=N)
+    l1.grid(column=1, row=1, sticky=N)
+    l2.grid(column=1, row=2, sticky=N)
+    l3.grid(column=4, row=1, sticky=N)
+    l4.grid(column=4, row=2, sticky=N)
+    l5.grid(column=5, row=1, sticky=N)
+    l6.grid(column=5, row=2, sticky=N)
+    askQuestion()
 
 root = Tk()
 root.title("Control")
 
-mainframe = ttk.Frame(root, padding="3 3 12 12")
+mainframe = ttk.Frame(root, padding="3 3 3 3")
 mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
 mainframe.columnconfigure(0, weight=1)
 mainframe.rowconfigure(0, weight=1)
 
 question = StringVar()
-rounds = StringVar()
+state = StringVar()
+cur_money = IntVar()
+bank = IntVar()
 
-ttk.Label(mainframe, textvariable=rounds).grid(column=1, row=1, sticky=N)
-ttk.Label(mainframe, textvariable=question).grid(column=2, row=1, sticky=N)
+l1 = ttk.Label(mainframe, textvariable=state, width=100, background='red')
+l2 = ttk.Label(mainframe, textvariable=question, width=100)
+l3 = ttk.Label(mainframe, text='Money: ')
+l4 = ttk.Label(mainframe, text='Bank: ')
+l5 = ttk.Label(mainframe, textvariable=cur_money)
+l6 = ttk.Label(mainframe, textvariable=bank)
 
-ttk.Button(mainframe, text="Correct", command=correctAns).grid(column=1, row=2, sticky=N)
-ttk.Button(mainframe, text="Incorrect", command=incorrectAns).grid(column=1, row=3, sticky=N)
-ttk.Button(mainframe, text="Time Up", command=time).grid(column=2, row=2, sticky=N)
-ttk.Button(mainframe, text="Bank", command=bank).grid(column=2, row=3, sticky=N)
-
-for child in mainframe.winfo_children():
-    child.grid_configure(padx=5, pady=5)
+startB = ttk.Button(mainframe, text="Start", command=start)
+startB.grid(column=1, row=2, sticky=N)
+correctB = ttk.Button(mainframe, text="Correct", command=correctAns)
+incorrectB = ttk.Button(mainframe, text="Incorrect", command=incorrectAns)
+timeB = ttk.Button(mainframe, text="Time Up", command=timeEvent)
+bankB = ttk.Button(mainframe, text="Bank", command=bankEvent)
 
 def importQuestions(file):
     global questions
@@ -67,13 +87,16 @@ def askQuestion():
     cntContestants = len(contestants) + 1
     if cntQuestions < len(questions):
         if cntRquestions == 1:
-            rounds.set('Round ' + str(cntRounds) + ' starting')
-            print('Round ' + str(cntRounds) + ' starting')
+            state.set('Round ' + str(cntRounds) + ' starting')
+            print(state.get())
+            question.set('')
+            mainframe.update()
             time.sleep(1)
-        print('Round ' + str(cntRounds) + ' Question ' + str(cntRquestions))
-        rounds.set('Round ' + str(cntRounds) + ' Question ' + str(cntRquestions))
+            cur_money.set(money[correct])
+        state.set('Round ' + str(cntRounds) + ' Question ' + str(cntRquestions))
+        print(state.get())
         question.set(questions[cntQuestions][0])
-        print(questions[cntQuestions][0])
+        print(question.get())
     else:
         print('So this is Embarasing')
         print('We seam to have run out of questions')
@@ -84,60 +107,42 @@ def questionHandler(event):
     global money, questions, contestants, mainQ, cntQuestions, correct, cntRounds, cntRquestions, bank
     if event == 1:
         print('Correct')
-        rounds.set('Correct')
+        state.set('Correct')
         correct += 1
     elif event == 2:
         print('Incorrect - ' + questions[cntQuestions][1])
-        rounds.set('Incorrect - ' + questions[cntQuestions][1])
         correct = 0
     elif event == 3:
-        bank += money[correct]
         print('Banked £' + str(money[correct]))
-        rounds.set('Banked £' + str(money[correct]))
-        time.sleep(1)
-        print('£' + str(bank) + ' now in bank')
-        rounds.set(('£' + str(bank) + ' now in bank'))
-        time.sleep(1)
+        print('£' + str(bank.get()) + ' now in bank')
+        bank.set(bank.get() + money[correct])
         correct = 0
         print('You now have £0')
-        rounds.set('You now have £0')
-        time.sleep(1)
-        rounds.set('Round ' + str(cntRounds) + ' Question ' + str(cntRquestions))
+        cur_money.set(money[correct])
+        mainframe.update()
         cntQuestions =- 1
         return
     elif event == 4:
         print('Time Up')
-        rounds.set('Time Up')
-        time.sleep(1)
-        print('You have £' + str(bank) + ' in the bank')
-        rounds.set('You have £' + str(bank) + ' in the bank')
+        print('You have £' + str(bank.get()) + ' in the bank')
         cntRounds += 1
         correct = cntRquestions = 0
     event = ''
     cntRquestions += 1
     cntQuestions += 1
     print('You now have £' + str(money[correct]))
-    time.sleep(1)
-    rounds.set('You now have £' + str(money[correct]))
-    time.sleep(1)
+    cur_money.set(money[correct])
     if correct == len(money) - 1:
         print('You have got all questions in round ' + str(cntRounds) + ' correct')
-        rounds.set('You have got all questions in round ' + str(cntRounds) + ' correct')
-        time.sleep(1)
         cntRounds += 1
         cntRquestions = 1
         correct = 0
-    if cntRquestions == 1:
-        time.sleep(1)
-        print('You must now choose the Weakest Link')
-        rounds.set('You must now choose the Weakest Link')
-        i = 1
-        while i - 1 < len(contestants):
-            print(str(i) + '\t' + contestants[i-1])
-            i += 1
-        contestants.remove(contestants[int(input('Please enter a selection (1 - ' + str(len(contestants)) + ')\n')) - 1])
-    time.sleep(1)
-    rounds.set('Round ' + str(cntRounds) + ' Question ' + str(cntRquestions))
+##    if cntRquestions == 1:
+##        print('You must now choose the Weakest Link')
+##        i = 1
+##        while i - 1 < len(contestants):
+##            print(str(i) + '\t' + contestants[i-1])
+##            i += 1
+##        contestants.remove(contestants[int(input('Please enter a selection (1 - ' + str(len(contestants)) + ')\n')) - 1])
 
-askQuestion()
 root.mainloop()
