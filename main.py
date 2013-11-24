5ab3ce202e9d5a113a84855f63c338dcf50f2fe5
william1616 <william1616@users.noreply.github.com>
2013-11-24 17:05:16 +0000

    cleanup main (control program)
    reduce number of threads, listner thread now can be stop/started,
    cleaned up global variables - possibly add config file?, count the
    number of questions imported, move all the TK and socket setup into
    functions, move receive command into function

--- main.py
+++ main.py
@@ -1,19 +1,7 @@
 from tkinter import *
 from tkinter import ttk
-import csv
-import socket
-import hashlib
-import threading
-import time
-import select
-import sys
-import json
+import csv, socket, hashlib, threading, time, select, sys, json
 
-window_title = 'The Weakest Link'
-status_lines = 25
-mainQ = 'questions.csv'
-finalQ = 'questions.csv'
-receive = ''
 variables = {}
 variables['cntQuestions'] = 0
 variables['correct'] = 0
@@ -26,145 +14,156 @@ variables['money'] = [0, 50,100,200,300,400,500,1000,2500,5000]
 variables['crtContestant'] = -1
 variables['gamemode'] = 0
 #0 = starting, 1 = questions, 2 = voting, 3 = contestant succesfully removed
-questions = []
 status = []
-questions = []
 peripherals = []
-bindAddresses = ['localhost']
-bindPort = 1024
-receivedCommand = ''
 
-serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-for address in bindAddresses:
-    serversocket.bind((address, bindPort))
-serversocket.listen(5)
-serversocket.setblocking(False)
+def initServer():
+    #server settings
+    bindAddress = 'localhost'
+    bindPort = 1024
+
+    #bindAddresses.append(socket.gethostname())
+    #bindAddresses.append(socket.gethostbyname(socket.gethostname()))
+    
+    status.append('Server bound to ' + bindAddress)
+    status_update()
+    
+    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    serversocket.bind((bindAddress, bindPort))
+    serversocket.listen(5)
+    serversocket.setblocking(False)
+    
+    return serversocket
 
 def status_update():
+    global displayStatus
+    #status settings
+    status_lines = 25
+    
+    #rewrite this function to use less memory - delete old results
     i = 0
-    start_status.set('')
+    displayStatus.set('')
     while True:
         if i == len(status): break
         if i > len(status) - status_lines:
-            start_status.set(start_status.get()+status[i]+'\n')
+            displayStatus.set(displayStatus.get()+status[i]+'\n')
         i += 1
 
-class connectListner (threading.Thread):
+class serverListner (threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
-        self.end = False
+        self.running = self.end = False
     def run(self):
-        while True:
-            print('server loop')
-            readable, writable, err = select.select([serversocket.fileno()], [], [], 1)
-            if readable:
-                clientsocket, address = serversocket.accept()
-                status.append(address[0] + ' Succesfully Connected')
-                status_update()
-                peripherals.append(clientsocket)
-            if self.end == True:
-                break
+        global peripherals
+        serversocket = initServer()
+        while not self.end:
+            while self.running:
+                readable, writable, err = select.select([serversocket.fileno()], [], [], 1)
+                if readable:
+                    clientsocket, address = serversocket.accept()
+                    status.append(address[0] + ' Succesfully Connected')
+                    status_update()
+                    peripherals.append(clientsocket)
+    def startListner(self):
+        if not self.running:
+            status.append('Started Listner')
+            self.running = True
+        else:
+            status.append('Cannot start Listner - Listner is already Running!')
+        status_update()
+    def stopListner(self, join):
+        if join and self.isAlive():
+            self.join()
+            status.append('Terminated listner Thread')
+        elif self.running and self.isAlive():
+            self.running = False
+            status.append('Stopped Listner')
+        else:
+            status.append('Cannot stop Listner - Listner is not Running')
+        status_update()
     def join(self):
-            self.end = True
-
-class receiveCommand (threading.Thread):
-    def __init__(self, clientsocket):
-        threading.Thread.__init__(self)
-        self.clientsocket = clientsocket
-        self.end = False
-    def run(self):
-        global receivedCommand
-        while True:
-            print('server receive loop')
-            readable, writable, err = select.select([self.clientsocket.fileno()], [], [], 1)
-            if readable:
-                msg = self.clientsocket.recv(4096)
-                #check = self.clientsocket.recv(40).decode('UTF-8')
-                #assert(hashlib.sha1(msg).hexdigest() == check)
-                receivedCommand = json.loads(msg.decode('UTF-8'))
-            if self.end == True:
-                break
-    def join(self):
-            self.end = True
+        self.end = True
+        self.running = False
+        threading.Thread.join(self)
 
 class questionControl(threading.Thread):
-    def __init__(self, ):
+    def __init__(self):
         threading.Thread.__init__(self)
-        self.end = False
+        self.newQuestion = False
     def run(self):
-        global receivedCommand
         while True:
-            askQuestion()
-            i = 0
-            while True:
-                if self.end == True:
-                    break
-                if receivedCommand != '':
-                    if isinstance(receivedCommand, int) and receivedCommand > 0 and receivedCommand <= 4:
-                        if questionHandler(receivedCommand) == True:
-                            break
-                    receivedCommand = ''
-            if self.end == True:
-                break
-    def join(self):
-            self.end = True
+            self.question, self.awnser = askQuestion()
+            self.newQuestion = False
+            while not self.newQuestion:
+                receivedCommand = receiveCommand()
+                if receivedCommand != '' and isinstance(receivedCommand, int) and receivedCommand > 0 and receivedCommand <= 4:
+                    if questionHandler(receivedCommand, self.question, self.awnser) == True:
+                        self.newQuestion = True
+
+def receiveCommand():
+    global peripherals, root, displayStatus, startFrame, mainFrame, listner
+    while True:
+        for clientsocket in peripherals:
+            readable, writable, err = select.select([clientsocket.fileno()], [], [], 0.1)
+            if readable:
+                msg = clientsocket.recv(4096)
+                #check = self.clientsocket.recv(40).decode('UTF-8')
+                #assert(hashlib.sha1(msg).hexdigest() == check)
+                return json.loads(msg.decode('UTF-8'))
 
 def start():
-    global peripherals, variables, mainQ
-    peripheralThreads = []
-    importQuestions(mainQ)
-    disconnect()
+    global variables
+    listner.stopListner(True)
     startFrame.grid_remove()
     mainFrame.grid()
     variables['gamemode'] = 1
-    for i in peripherals:
-        peripheralThreads.append(receiveCommand(i))
-    for i in peripheralThreads:
-        i.start()
-    run = questionControl()
-    run.start()
+    questionThread = questionControl()
+    questionThread.start()
 
-def connect():
-        global listner
-        listner = connectListner()
-        listner.start()
-        status.append('Started Listner')
-        status_update()
-        
-def disconnect():
+def initListner():
     global listner
-    if listner.isAlive():
-        listner.join()
-        status.append('Stopped Listner')
-        status_update()
+    listner = serverListner()
+    listner.start()
 
-root = Tk()
-root.title(window_title)
+def initTk():
+    global displayStatus, root, startFrame, mainFrame, listner
+    #Tk settings
+    window_title = 'The Weakest Link'
+    
+    root = Tk()
+    root.title(window_title)
 
-startFrame = ttk.Frame(root, padding="3 3 3 3")
-startFrame.grid(column=0, row=0, sticky=(N, W, E, S))
-startFrame.columnconfigure(0, weight=1)
-startFrame.rowconfigure(0, weight=1)
+    startFrame = ttk.Frame(root, padding="3 3 3 3")
+    startFrame.grid(column=0, row=0, sticky=(N, W, E, S))
+    startFrame.columnconfigure(0, weight=1)
+    startFrame.rowconfigure(0, weight=1)
 
-mainFrame = ttk.Frame(root, padding="3 3 3 3")
-mainFrame.grid(column=0, row=0, sticky=(N, W, E, S))
-mainFrame.columnconfigure(0, weight=1)
-mainFrame.rowconfigure(0, weight=1)
-mainFrame.grid_forget()
+    mainFrame = ttk.Frame(root, padding="3 3 3 3")
+    mainFrame.grid(column=0, row=0, sticky=(N, W, E, S))
+    mainFrame.columnconfigure(0, weight=1)
+    mainFrame.rowconfigure(0, weight=1)
+    mainFrame.grid_forget()
 
-start_status = StringVar()
+    displayStatus = StringVar()
 
-ttk.Label(startFrame, text='Status').grid(column=1, row=1, sticky=N)
-ttk.Label(startFrame, textvariable=start_status, width=100).grid(column=1, row=2, sticky=N)
-ttk.Button(startFrame, text='Start', command=start).grid(column=2, row=1, sticky=N)
-ttk.Button(startFrame, text='Start Listner', command=connect).grid(column=2, row=2, sticky=N)
-ttk.Button(startFrame, text='Stop Listner', command=disconnect).grid(column=2, row=3, sticky=N)
+    ttk.Label(startFrame, text='Status').grid(column=1, row=1, sticky=N)
+    ttk.Label(startFrame, textvariable=displayStatus, width=100).grid(column=1, row=2, sticky=N)
+    ttk.Button(startFrame, text='Start', command=start).grid(column=2, row=1, sticky=N)
+    ttk.Button(startFrame, text='Start Listner', command=lambda: listner.startListner()).grid(column=2, row=2, sticky=N)
+    ttk.Button(startFrame, text='Stop Listner', command=lambda: listner.stopListner(False)).grid(column=2, row=3, sticky=N)
 
-ttk.Label(mainFrame, text='Status', width=100).grid(column=1, row=1, sticky=N)
-ttk.Label(mainFrame, textvariable=start_status, width=100).grid(column=1, row=2, sticky=N)
+    ttk.Label(mainFrame, text='Status', width=100).grid(column=1, row=1, sticky=N)
+    ttk.Label(mainFrame, textvariable=displayStatus, width=100).grid(column=1, row=2, sticky=N)
 
 def askQuestion():
-    global questions, variables
+    global variables
+    #question settings
+    mainQ = 'questions.csv'
+    
+    try:
+        questions
+    except:
+        questions = importQuestions(mainQ)
     if variables['crtContestant'] < len(variables['contestants']) - 1:
         variables['crtContestant'] += 1
     else:
@@ -184,6 +183,8 @@ def askQuestion():
         status.append(list(variables['contestants'].keys())[variables['crtContestant']] + ': ' + variables['question'])
         status_update()
         updateClient()
+                # return question, awnser
+        return questions[variables['cntQuestions']][0], questions[variables['cntQuestions']][1]
     else:
         status.append('So this is Embarasing')
         status.append('We seam to have run out of questions')
@@ -191,14 +192,14 @@ def askQuestion():
         status_update()
         sys.exit()
 
-def questionHandler(event):
-    global questions, variables, receivedCommand
+def questionHandler(event, question, awnser):
+    global variables
     if event == 1:
         status.append('Correct')
         variables['correct'] += 1
         variables['contestants'][list(variables['contestants'].keys())[variables['crtContestant']]] += 1
     elif event == 2:
-        status.append('Incorrect - ' + questions[variables['cntQuestions']][1])
+        status.append('Incorrect - ' + awnser)
         variables['correct'] = 0
     elif event == 3:
         variables['bank'] += variables['money'][variables['correct']]
@@ -234,16 +235,14 @@ def questionHandler(event):
             i += 1
         status_update()
         updateClient()
-        receivedCommand = ''
         while True:
-            if receivedCommand != '':
-                if isinstance(receivedCommand, int) and receivedCommand > 0 and receivedCommand <= len(variables['contestants']):
-                    status.append(list(variables['contestants'].keys())[receivedCommand - 1] + ' you are the Weakest Link! Goodbye')
-                    variables['contestants'].pop(list(variables['contestants'].keys())[receivedCommand - 1])
-                    variables['gamemode'] = 3
-                    updateClient()
-                    break
-                receivedCommand = ''
+            receivedCommand = receiveCommand()
+            if receivedCommand != '' and isinstance(receivedCommand, int) and receivedCommand > 0 and receivedCommand <= len(variables['contestants']):
+                status.append(list(variables['contestants'].keys())[receivedCommand - 1] + ' you are the Weakest Link! Goodbye')
+                variables['contestants'].pop(list(variables['contestants'].keys())[receivedCommand - 1])
+                variables['gamemode'] = 3
+                updateClient()
+                break
         variables['gamemode'] = 1
     receivedCommand = ''
     status_update()
@@ -251,17 +250,24 @@ def questionHandler(event):
     return True
 
 def importQuestions(file):
-    global questions
+    status.append('Importing Questions...')
+    status_update()
+    questions = []
+    cnt = 0
     with open(file) as csvfile: # add search directory for csv
         questionfile = csv.reader(csvfile)
         for row in questionfile:
             # where row[0] = questions & row[1] = awnser
             if row[0] and row[1]:
                 questions.append([row[0], row[1]])
-    # with statement automatically closes the csv file cleanly even in event of unexpected script termination
+                cnt += 1
+        status.append('Imported ' + str(cnt) + ' Questions')
+        status_update()
+		# with statement automatically closes the csv file cleanly even in event of unexpected script termination
+		return questions
 
 def updateClient():
-    global variables
+    global variables, peripherals
     jsonVariables = json.dumps(variables)
     assert(variables == json.loads(jsonVariables))
     jsonBytes = jsonVariables.encode('UTF-8')
@@ -269,5 +275,8 @@ def updateClient():
     for clientsocket in peripherals:
         bytesSent = clientsocket.send(jsonBytes)
         #bytesSent = clientsocket.send(check)
-    
-root.mainloop()
+
+if __name__ == '__main__':
+    initTk()
+    initListner()
+    root.mainloop()
