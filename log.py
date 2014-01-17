2219a20745f0628d098ac1236b41b03b3dcc469f
william1616 <william1616@users.noreply.github.com>
2014-01-17 20:40:27 +0000

    Finish log API validate config

--- log.py
+++ log.py
@@ -1,6 +1,7 @@
 from datetime import datetime
-from json import load
+from json import load, dump
 from os.path import basename
+import __main__
 
 def initConfig():    
     #config settings
@@ -8,24 +9,33 @@ def initConfig():
     options = {"Tk": {"window_title": "The Weakest Link", "status_lines": 25}, "questions": {"mainQ": "questions.csv", "finalQ": "questions.csv"}, "server": {"bindPort": 1024, "bindAddress": "localhost"}, "debug": {"fileName": "log.txt", "log": True}}
     
     with open(fileName, 'r') as configFile:
-        config = load(configFile)
-        if not isinstance(config, dict):
-            raise KeyError('ConfigFile is not a dictionary')
-        for key in config.values():
-            print(str(key))
-            print(str(type(key)))
-            if isinstance(key, dict):
-                for subkey in key.values():
-                    if not isinstance(subkey, str) or not subkey in options[key]:
-                        raise KeyError('Key [' + str(key) + '][' + str(subkey) +'] not found in configFile')
-            elif isinstance(key, str):
-                if not isinstance(key, str) or not key in options[key]:
-                    raise KeyError('Key [' + str(key) + '] not found in configFile')
-        return config
+        try:
+            config = load(configFile)
+            if not isinstance(config, dict):
+                print('ConfigFile is not a Dictionary')
+                raise
+            for key in options:
+                value = options.get(key)
+                if key in config:
+                    for subkey in value:
+                        subvalue = value.get(subkey)
+                        if not subkey in config[key]:
+                            print('cannot find key [' + key + '][' + subkey + ' in ConfigFile')
+                            raise
+                else:
+                    print('cannot find key [' + key + '] in ConfigFile')
+                    raise
+                return config
+        except:
+            print('Overwritting config file')
+            with open(fileName, 'w') as configFile:
+                dump(options, configFile, indent=4)
+            return options
 
 config = initConfig()
+logName = basename(__file__)
 
 def log(text, fileName=config['debug']['fileName']):
     if config['debug']['log']:
         with open(fileName, 'a') as file:
-            file.write(str(datetime.daynow()) + ' [' + basename(__file__) + '] ' + text + '\n')
+            file.write(str(datetime.now()) + ' [' + logName + '] ' + text + '\n')
