d14535641477a1f6c06b28199ce467b72f271106
william1616 <william1616@users.noreply.github.com>
2014-02-22 15:19:38 +0000

    update build script

--- setup.py
+++ setup.py
@@ -1,48 +1,49 @@
 from cx_Freeze import setup, Executable
-import sys, datetime, json, collections
+import sys, datetime, json, os
 
 with open('build\\buildData.json', 'r') as file:
-	buildData = json.load(file)
+    buildData = json.load(file)
 
 if input('Release Build [n]: ') == 'y':
-	temp = input('Version: [' + buildData['version'] + ']: ')
-	if temp: buildData['version'] = temp
-	versionName = 'Build ' + str(buildData['buildNo']) + ' [' + buildData['version'] + ']'
+    temp = input('Version: [' + str(buildData['version']) + ']: ')
+    if temp: buildData['version'] = temp
+    versionName = 'Build ' + str(buildData['buildNo']) + ' [' + buildData['version'] + ']'
 
-	versionList = buildData['version'].split('.')
-	newVersionEnd = str(int(versionList[len(versionList) - 1]) + 1)
-	newVersion = '.'.join(versionList[:len(versionList) - 1])
-	newVersion = newVersion + '.' + newVersionEnd
+    versionList = buildData['version'].split('.')
+    newVersionEnd = str(int(versionList[len(versionList) - 1]) + 1)
+    newVersion = '.'.join(versionList[:len(versionList) - 1])
+    newVersion = newVersion + '.' + newVersionEnd
 
-	buildData['version'] = newVersion
+    buildData['version'] = newVersion
 else:
-	versionName = 'Build ' + str(buildData['buildNo'])
+    versionName = 'Build ' + str(buildData['buildNo'])
 
 buildData['buildNo'] += 1
 
 with open('build\\buildData.json', 'w') as file:
-	json.dump(buildData, file, indent=4)
-	
+    json.dump(buildData, file, indent=4)
+    
 # Dependencies are automatically detected, but it might need
 # fine tuning.
-buildOptions = dict(packages = ['csv', 'threading', 'time', 'sys', 'json', 'os', 'network', 'datetime'], 
-	excludes = [], 
-	path = sys.path, 
-	include_files = [('config.json',''), 
-	('questions.csv','')],
-	build_exe = 'build\\' + datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S') + ' ' + versionName)
+buildOptions = dict(packages = ['network', 'misc', 'control', 'gui', 'server', 'tkinter', 'csv', 'threading', 'time', 'sys', 'json', 'os', 'network', 'datetime', 'math', 'operator', 'pygame', 'collections', 'hashlib'], 
+    excludes = [], 
+    path = sys.path.extend(os.path.dirname(os.path.abspath(__file__))),
+    include_files = [('config.json',''), ('questions.csv',''), ('redPlaceholder.png',''), ('bluePlaceholder.png','')],
+    create_shared_zip = True,
+    compressed = True,
+    build_exe = 'build\\' + datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S') + ' ' + versionName)
 
 import sys
 base = 'Win32GUI' if sys.platform=='win32' else None
 
 executables = [
     Executable('main.py', 
-	base=base, 
-	targetName = 'The Weakest Link.exe')
+    base=base, 
+    targetName = 'The Weakest Link.exe')
 ]
 
 setup(name='The Weakest Link',
-	  version = versionName,
-	  description = 'The Weakest Link Game',
-	  options = dict(build_exe = buildOptions),
-	  executables = executables)
+      version = versionName,
+      description = 'The Weakest Link Game',
+      options = dict(build_exe = buildOptions),
+      executables = executables)
