5e89b2a7e4176a2ea850ba31cb38baa2765e26a1
william1616 <william1616@users.noreply.github.com>
2014-09-02 20:20:42 +0100

    [setup.py] Revert to using cx_freeze for #22

--- setup.py
+++ setup.py
@@ -1,19 +1,49 @@
-from distutils.core import setup
-from glob import glob
-import py2exe, sys    
+from cx_Freeze import setup, Executable
+import sys, datetime, json, os
 
-sys.path.extend(['.', './/weakest_link'])
+with open('build//buildData.json', 'r') as file:
+    buildData = json.load(file)
 
-buildOptions = {'optimize': 2,
-                'bundle_files': 2,
-                }
-                
-dataFiles = [('resources', glob('./resources/*')),
-             ('.', ['./config.json'])
-			 ]
+if input('Release Build [n]: ') == 'y':
+    temp = input('Version: [' + str(buildData['version']) + ']: ')
+    if temp: buildData['version'] = temp
+    versionName = 'Build ' + str(buildData['buildNo']) + ' [' + buildData['version'] + ']'
 
-setup(windows=['main.py'], 
-      options={'py2exe': buildOptions},
-      data_files=dataFiles,
-	  zipfile=None
-	  )
+    versionList = buildData['version'].split('.')
+    newVersionEnd = str(int(versionList[len(versionList) - 1]) + 1)
+    newVersion = '.'.join(versionList[:len(versionList) - 1])
+    newVersion = newVersion + '.' + newVersionEnd
+
+    buildData['version'] = newVersion
+else:
+    versionName = 'Build ' + str(buildData['buildNo'])
+
+buildData['buildNo'] += 1
+
+with open('build//buildData.json', 'w') as file:
+    json.dump(buildData, file, indent=4)
+    
+# Dependencies are automatically detected, but it might need
+# fine tuning.
+buildOptions = dict(packages = ['network', 'misc', 'control', 'gui', 'server', 'prompt', 'tkinter', 'csv', 'threading', 'time', 'sys', 'json', 'os', 'network', 'datetime', 'math', 'operator', 'pygame', 'collections', 'hashlib', 'socket', 'select'], 
+    excludes = [], 
+    path = sys.path.extend(['.', './/weakest_link']),
+    include_files = [('config.json',''), ('resources//questions.csv','resources//questions.csv'), ('resources//redPlaceholder.png','resources//redPlaceholder.png'), ('resources//bluePlaceholder.png','resources//bluePlaceholder.png'), ('resources//correctBlue.png','resources//correctBlue.png'), ('resources//incorrectRed.png','resources//incorrectRed.png'), ('resources//neutralBlue.png','resources//neutralBlue.png')],
+    create_shared_zip = True,
+    compressed = True,
+    build_exe = 'build//' + datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S') + ' ' + versionName)
+
+import sys
+base = 'Win32GUI' if sys.platform=='win32' else None
+
+executables = [
+    Executable('main.py', 
+    base=base, 
+    targetName = 'The Weakest Link.exe')
+]
+
+setup(name='The Weakest Link',
+      version = versionName,
+      description = 'The Weakest Link Game',
+      options = dict(build_exe = buildOptions),
+      executables = executables)
